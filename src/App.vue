1da6783211a55dd4640bccce2831c45c5f9798e8
StephenBrooks220413 <stephen.brooks220413@gmail.com>
2020-11-12 10:20:44 -0600

    updated home page

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,84 @@
 <template>
-  <!-- <div id="app"> -->
-    <v-app>
-      <!-- <div id="nav"> -->
-        <v-app-bar app>
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-              <a class="navbar-brand" href="#">Navbar</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+<!-- <div id="app"> -->
+<v-app>
+    <!-- <div id="nav"> -->
+    <v-app-bar app>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul class="navbar-nav">
-                  <router-link class="nav-link nav-item" to="/">Home</router-link> 
-                  <router-link class="nav-link nav-item" to="/about">About</router-link> 
-                  <router-link class="nav-link nav-item" to="/contact">Contact</router-link> 
-                  <router-link class="nav-link nav-item" to="/catalog">Catalog</router-link> 
-                  <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      Dropdown link
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                      <a class="dropdown-item" href="#">Action</a>
-                      <a class="dropdown-item" href="#">Another action</a>
-                      <a class="dropdown-item" href="#">Something else here</a>
-                    </div>
-                  </li>
+                    <router-link class="nav-link nav-item" to="/">Home</router-link>
+                    <router-link class="nav-link nav-item" to="/about">About</router-link>
+                    <router-link class="nav-link nav-item" to="/contact">Contact</router-link>
+                    <router-link class="nav-link nav-item" to="/catalog">Catalog</router-link>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Dropdown link
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
                 </ul>
-              </div>
-            </nav>
-          </v-app-bar>
-        <v-main>
-          <router-view />
-          <v-container fluid>
-            
-          </v-container>
-          
-           <v-navigation-drawer app>
-          </v-navigation-drawer>
-          
-        </v-main>
-      <!-- </div> -->
-      <v-footer app>
-        <!--  -->
-      </v-footer>
-    </v-app>
-  <!-- </div> -->
+            </div>
+        </nav>
+    </v-app-bar>
+    <v-main>
+        <router-view />
+        <v-container fluid>
+
+        </v-container>
+
+        <v-navigation-drawer app>
+        </v-navigation-drawer>
+
+    </v-main>
+    <!-- </div> -->
+    <v-footer app>
+
+    </v-footer>
+</v-app>
+<!-- </div> -->
 </template>
 
 <style lang="scss">
 @import './components/bootstrap.min.css';
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 
 #nav {
-  padding: 30px;
+    padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    a {
+        font-weight: bold;
+        color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
+        &.router-link-exact-active {
+            color: #42b983;
+        }
     }
-  }
+}
+
+footer {
+    background-color: black;
+    color: teal !important;
+    bottom: 0;
+    padding-top: 40px;
+    padding-bottom: 50px;
+}
+
+.footer h3 {
+    color: white !important;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,77 @@
 <template>
-  <div class="home">
-    
-  </div>
+<div class="home">
+    <v-container>
+        <br><br><br>
+        <h1 class="display-4 text-center">Technology & Programming</h1>
+        <br>
+        <div class="text-center">
+            <img src="../assets/logo.png" class="img" width="110px" height="90px" alt="logo">
+            <img src="../assets/js-logo.png" class="img" width="110px" height="90px" alt="logo">
+            <img src="../assets/react-logo.png" class="img" width="110px" height="90px" alt="logo">
+            <img src="../assets/asp.net_.png" class="img" width="110px" height="90px" alt="logo">
+        </div>
+        <br>
+        <div class="container content">
+            <div class="row text-center">
+                <div col-md-6>
+                    <h1>Making headway</h1>
+                    <p class="lead">upcoming technology for layouts are including C# and Vue.js</p>
+                </div>
+                <div class="col-md-6">
+                    <a class="btn btn-success" href="mailto:stephen.brooks220413@gmail.com" target="_blank">MORE INFO</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque tempore saepe in</h4>
+                </div>
+                <div class="col-md-4">
+                    <h4>inventore! Ut explicabo enim minima beatae ex dolorem deserunt molestias repellendus omnis!</h4>
+                </div>
+                <div class="col-md-4">
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque tempore saepe in</h4>
+                </div>
+            </div>
+        </div>
+        <br><br><br><br><br>
+    </v-container>
+</div>
 </template>
 
+<style>
+body {
+    background: rgb(2, 0, 36);
+    background: radial-gradient(circle, rgba(2, 0, 36, 0.4770950616574755) 0%, rgba(235, 238, 238, 1) 67%);
+}
+
+.img {
+    padding-right: 30px;
+}
+
+.app-bar {
+    height: 60vh;
+    width: 350px;
+    margin-left: auto;
+    margin-right: auto;
+}
+
+.content {
+    margin-top: 40px;
+    margin-bottom: 60px;
+}
+
+h1 {
+    color: teal !important;
+}
+
+.btn {
+    margin-top: 30px;
+    margin-bottom: 80px;
+    font-size: 1.8em !important;
+    padding-top: 10px;
+    padding-bottom: 10px;
+}
+</style>
